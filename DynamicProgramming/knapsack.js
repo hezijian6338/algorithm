4f30b073f833399cc7154a3eaaec5a561e438219
hezijian6338 <hezj@ctjt.cn>
2020-10-16 09:38:49 +0800

    0-1背包问题的备忘录 js版本

--- DynamicProgramming/knapsack.js
+++ DynamicProgramming/knapsack.js
@@ -6,6 +6,7 @@ class Knapsack {
     constructor() {
         this.result = 0
         this.max = 0
+        this.memo = new Map()
     }
 
     backtrack(N, W, wt, val, i) {
@@ -26,8 +27,31 @@ class Knapsack {
         this.backtrack(N - 1, W, wt, val, i + 1)
         this.result -= 0
     }
+
+    dp(N, W, wt, val, i) {
+        if (N <= 0 || W <= 0) {
+            return 0
+        }
+
+        let key = W + "," + i
+        if (this.memo.has(key)) {
+            return this.memo.get(key)
+        }
+
+        let res = 0
+
+        if (W - wt[i] >= 0) {
+            res = (this.dp(N - 1, W - wt[i], wt, val, i + 1) + val[i]) > (this.dp(N - 1, W, wt, val, i + 1)) ? (this.dp(N - 1, W - wt[i], wt, val, i + 1) + val[i]) : this.dp(N - 1, W, wt, val, i + 1)
+        } else {
+            res = this.dp(N - 1, W, wt, val, i + 1)
+        }
+
+        this.memo.set(key, res)
+        return res
+    }
 }
 
 ks = new Knapsack()
 ks.backtrack(3, 4, [2, 1, 3], [4, 2, 3], 0)
 console.log(ks.max)
+console.log(ks.dp(3, 4, [2, 1, 3], [4, 2, 3], 0))
