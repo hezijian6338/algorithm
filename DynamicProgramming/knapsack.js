b9cdd8d2ae17825a021ffb2b534ec9aff80a61c0
hezijian6338 <hezj@ctjt.cn>
2020-10-16 09:51:05 +0800

    0-1背包问题的 dp Table Js写法

--- DynamicProgramming/knapsack.js
+++ DynamicProgramming/knapsack.js
@@ -7,8 +7,10 @@ class Knapsack {
         this.result = 0
         this.max = 0
         this.memo = new Map()
+        this._dp = []
     }
 
+    // 回溯法
     backtrack(N, W, wt, val, i) {
         if (W <= 0 || N <= 0) {
             if (this.result > this.max) {
@@ -28,6 +30,7 @@ class Knapsack {
         this.result -= 0
     }
 
+    // 备忘录法
     dp(N, W, wt, val, i) {
         if (N <= 0 || W <= 0) {
             return 0
@@ -49,9 +52,42 @@ class Knapsack {
         this.memo.set(key, res)
         return res
     }
+
+    // dp Table法
+    knapsack(N, W, wt, val) {
+        for (let i = 0; i <= N; i++) {
+            let array = []
+            for (let j = 0; j <= W; j++) {
+                array.push(-1)
+            }
+            this._dp.push(array)
+        }
+
+        // basecase: 物品为空
+        for (let i = 0; i <= N; i++) {
+            this._dp[i][0] = 0
+        }
+
+        // basecase: 背包满了
+        for (let w = 0; w <= W; w++) {
+            this._dp[0][w] = 0
+        }
+
+        for (let i = 1; i <= N; i++) {
+            for (let w = 1; w <= W; w++) {
+                if (w - wt[i - 1] >= 0) {
+                    this._dp[i][w] = (this._dp[i - 1][w - wt[i - 1]] + val[i - 1]) > this._dp[i - 1][w] ? (this._dp[i - 1][w - wt[i - 1]] + val[i - 1]) : this._dp[i - 1][w]
+                } else {
+                    this._dp[i][w] = this._dp[i - 1][w]
+                }
+            }
+        }
+        return this._dp[N][W]
+    }
 }
 
 ks = new Knapsack()
 ks.backtrack(3, 4, [2, 1, 3], [4, 2, 3], 0)
 console.log(ks.max)
 console.log(ks.dp(3, 4, [2, 1, 3], [4, 2, 3], 0))
+console.log(ks.knapsack(3, 4, [2, 1, 3], [4, 2, 3]))
