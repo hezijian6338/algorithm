8e21f4e36595e48c2401cf8a5949166f8effdede
hezijian6338 <hezj@ctjt.cn>
2020-10-22 19:22:46 +0800

    最长子序列的回溯法 1.1的 java完整版, 备忘录法好像都不太行

--- DynamicProgramming/LengthOfLIS/lengthOfLIS.java
+++ DynamicProgramming/LengthOfLIS/lengthOfLIS.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
 class LengthOfLIS {
   public int max = 0;
-  public int tmp = 0;
+  public HashMap<Integer, Integer> memo = new HashMap<>();
 
   LengthOfLIS() {
     
@@ -27,6 +30,55 @@ class LengthOfLIS {
     }
   }
 
+  public int dp(int[] nums, int i, int tmp) {
+    this.max = 0;
+
+    if (this.memo.containsKey(i)) {
+      return this.memo.get(i);
+    }
+
+    if (tmp > this.max) {
+      this.max = tmp;
+    }
+
+    if (i == 0) {
+      this.memo.put(i, tmp);
+      System.out.println(this.memo);
+      return tmp;
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+      if (nums[j] < nums[i]) {
+        int res = (this.dp(nums, j, tmp) + 1);
+        return res;
+      } else {
+        return this.dp(nums, j, tmp);
+      }
+    }
+
+    return this.dp(nums, i, tmp);
+  }
+
+  public int lengthOfLIS(int[] nums) {
+    int[] dp = new int[nums.length];
+    Arrays.fill(dp, 1);
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[i] > nums[j]) {
+          dp[i] = dp[i] > (dp[j] + 1) ? dp[i] : (dp[j] + 1);
+        }
+      }
+    }
+
+    int res = 0;
+    for (int i = 0; i < nums.length; i++) {
+      res = res > dp[i] ? res : dp[i];
+    }
+    
+    return res;
+  }
+
   public static void main(String[] args) {
     LengthOfLIS lis = new LengthOfLIS();
 
@@ -35,5 +87,11 @@ class LengthOfLIS {
     lis.backtrack(nums, nums.length - 1, 1);
 
     System.out.println(lis.max);
+
+    System.out.println(lis.dp(nums, nums.length - 1, 1));
+
+    System.out.println(lis.lengthOfLIS(nums));
+
+    // System.out.println(lis.memo);
   }
 }
