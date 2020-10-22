70f12a20b513ff9fb21ccbab943b7386e29b8928
hezijian6338 <hezj@ctjt.cn>
2020-10-22 10:49:31 +0800

    添加选择排序算法的 python代码

--- Sort/SelectionSort/selectionSort.py
+++ Sort/SelectionSort/selectionSort.py
@@ -12,7 +12,21 @@ class SelectionSort(object):
                 nums[i] = tmp
         return nums
 
+    def Sort2(self, nums):
+        for i in range(len(nums) - 1, -1, -1):
+            mainJ = i
+            for j in range(i - 1, -1, -1):
+                if nums[j] < nums[mainJ]:
+                    mainJ = j
+
+            if i != mainJ:
+                tmp = nums[mainJ]
+                nums[mainJ] = nums[i]
+                nums[i] = tmp
+
+        return nums
 ss = SelectionSort()
 nums = [3, 5, 8, 5, 4, 3, 8, 8, 2, 4, 3, 3, 7, 4, 5]
 print(ss.Sort(nums))
+print(ss.Sort2(nums))
               
