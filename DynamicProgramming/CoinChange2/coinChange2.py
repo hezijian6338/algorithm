a7a7a989f39f18743e95c35a7a23b48b5b988661
hezijian6338 <hezj@ctjt.cn>
2020-10-30 17:57:21 +0800

    添加了零钱兑换2 的 Python版本的回溯法

--- DynamicProgramming/CoinChange2/coinChange2.py
+++ DynamicProgramming/CoinChange2/coinChange2.py
@@ -1,4 +1,28 @@
 class CoinChange2(object):
+
+    def __init__(self):
+        self.track = []
+        self.path = set()
+        self.res = 0
+
+    def backtrack(self, coins, rest):
+        if rest == 0:
+            self.track.sort()
+            if tuple(self.track) not in self.path:
+                self.res += 1
+                self.path.add(tuple(self.track))
+            return
+
+        for i in range(len(coins)):
+            if rest - coins[i] >= 0:
+                rest = rest - coins[i]
+                self.track.append(coins[i])
+                self.backtrack(coins, rest)
+                rest = rest + coins[i]
+                self.track.pop(-1)
+            else:
+                continue
+
     def CoinChange2(self, coins, target):
         dp = [[0]*(target + 1) for i in range(len(coins) + 1)]
         for i in range(target + 1):
@@ -19,6 +43,8 @@ cc2 = CoinChange2()
 coins = [1, 2, 5]
 target = 5
 print(cc2.CoinChange2(coins, target))
+cc2.backtrack(coins, target)
+print(cc2.res)
 
 
 
