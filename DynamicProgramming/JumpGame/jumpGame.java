dd38f20b0d8a8fd47b3ca366e05955d1c3cc6c75
hezijian6338 <hezj@ctjt.cn>
2020-11-10 16:42:09 +0800

    添加贪心算法之玩跳跃游戏的第二份 Java代码

--- DynamicProgramming/JumpGame/jumpGame.java
+++ DynamicProgramming/JumpGame/jumpGame.java
@@ -14,6 +14,28 @@ class JumpGame {
     return farthest >= n - 1;
   }
 
+  public int jump(int[] nums) {
+    int n = nums.length;
+    int end = 0;
+    int farthest = 0;
+    int jumps = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+      farthest = (nums[i] + i) > farthest ? (nums[i] + i) : farthest;
+
+      if (end == i) {
+        jumps++;
+        end = farthest;
+        // 若题目不保证一定能到终点, 则需要判断终止条件 (最远的距离所在的可跳步数是 0)
+        // if (farthest == i) {
+        //   return -1;
+        // }
+      }
+    }
+
+    return jumps;
+  }
+
   public static void main(String[] args) {
     JumpGame jg = new JumpGame();
     int[] nums = {2, 3, 1, 1, 4};
@@ -22,5 +44,6 @@ class JumpGame {
     boolean _res = jg.canJump(_nums);
     System.out.println(res);
     System.out.println(_res);
+    System.out.println(jg.jump(nums));
   }
 }
